95dabbc71244256dcd3f198673cb75e700f341c7
Dylan Karr <webmaster@dylankarr.com>
2014-12-16 11:38:29 -0500

    Switched to Rugged Gem
    I switched from the Ruby-Git gem to the Rugged gem as Ruby-Git is no
    longer maintained.

--- lib/stevenson/template_loaders/git_loader.rb
+++ lib/stevenson/template_loaders/git_loader.rb
@@ -1,16 +1,20 @@
-require 'git'
+require 'rugged'
 require 'tmpdir'
 
 module Stevenson
   module TemplateLoaders
     class GitLoader
       def initialize(template_url)
+        # Set the template_url for later use
         @template_url = template_url
       end
 
       def load
+        # Clone the repo to a temporary directory to work with
         dir = Dir.mktmpdir
-        Git::Base.clone @template_url, "#{dir}/repo"
+        Rugged::Repository.clone_at @template_url, "#{dir}/repo"
+
+        # Return the path to the temporary directory
         "#{dir}/repo"
       end
     end
