665692158a1ed7ca7a62d197f92d7dac230342b3
Dylan Karr <webmaster@dylankarr.com>
2014-12-16 13:02:03 -0500

    Fixed the Spec for Stevenson

--- spec/lib/stevenson_spec.rb
+++ spec/lib/stevenson_spec.rb
@@ -1,17 +1,44 @@
 describe Stevenson::Application do
-  context 'new' do
+  describe 'new' do
+    context 'with a valid repo' do
+      let(:directory_name) { Dir.mktmpdir }
+      let(:output) { capture(:stdout) { subject.new directory_name } }
+
+      it 'creates a directory at test_directory' do
+        expect(File.exists? directory_name).to eq true
+      end
+
+      after { FileUtils.remove_entry_secure directory_name }
+    end
+
     context 'with an invalid repo' do
       let(:directory_name) { 'test_directory' }
+      let(:output) { capture(:stdout) { subject.new directory_name } }
 
       before do
-        subject.options = { template: 'https://github.com/RootsRated/stevenson.git' }
+        subject.options = { template: 'https://github.com/RootsRated/notarepo.git' }
       end
 
-      it 'exits' do
-        expect{
-          subject.new directory_name
-        }.to raise_exception SystemExit
+      it 'does not create a directory at test_directory' do
+        expect(File.exists? directory_name).to eq false
+      end
+
+      it 'outputs an error message' do
+        expect(output).to include 'No git repository could be found at the provided URL.'
       end
     end
+  end
+
+  def capture(stream)
+    begin
+      stream = stream.to_s
+      eval "$#{stream} = StringIO.new"
+      yield
+      result = eval("$#{stream}").string
+    ensure
+      eval("$#{stream} = #{stream.upcase}")
+    end
+  
+    result
   end
 end
