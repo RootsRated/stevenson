61b150fcb69d0e75d38e5ca50c044453efda521c
Dylan Karr <webmaster@dylankarr.com>
2014-12-23 11:19:07 -0500

    Added an Option to Switch Branches

--- lib/stevenson.rb
+++ lib/stevenson.rb
@@ -10,9 +10,12 @@ module Stevenson
   class Application < Thor
     desc 'stevenson new PROJECT_NAME', 'generates a Jekyll at PROJECT_NAME'
 
+    method_option :branch,
+                  aliases: '-b',
+                  desc: 'The git branch you would like to use from your template'
     method_option :jekyll,
                   type: :boolean,
-                  aliases: "-j",
+                  aliases: '-j',
                   desc: 'Jekyll compiles the output directory'
     method_option :template,
                   aliases: '-t',
@@ -26,6 +29,9 @@ module Stevenson
     def new(output_directory)
       # Load the template using the template loader
       template = Stevenson::TemplateLoader.load options[:template]
+
+      # If a branch is provided, switch to that branch
+      template.switch_branch options[:branch]
 
       # Configure the template
       configurator = Stevenson::Configurator::YAMLConfigurator.new template.path
