ce5b53f2d5399e762a0d912ef93f75e1fc3bc553
Scott BonAmi <sbonami@me.com>
2015-09-28 22:43:37 -0400

    Clean-up Template class selection

--- lib/stevenson/template/loader.rb
+++ lib/stevenson/template/loader.rb
@@ -10,11 +10,15 @@ module Stevenson
       def template
         return Template.load(matching_alias.name, matching_alias.options) if matching_alias?
 
+        template_klass.new template_name, options
+      end
+
+      def template_klass
         case template_name
         when /^.*\.git$/
-          Template::Git.new template_name
+          Template::Git
         else
-          Template::Local.new template_name
+          Template::Local
         end
       end
 
