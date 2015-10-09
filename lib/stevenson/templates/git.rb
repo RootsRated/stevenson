bb558463f91a535f19dfac2e30a38c18187b517f
Scott BonAmi <sbonami@me.com>
2015-10-09 15:13:54 -0400

    Utilize local template with Git for use of subdirectory option

--- lib/stevenson/templates/git.rb
+++ lib/stevenson/templates/git.rb
@@ -2,7 +2,7 @@ require 'git'
 
 module Stevenson
   module Template
-    class Git < Base
+    class Git < Local
       attr_reader :template_url, :options
 
       def initialize(template_url, options)
@@ -10,16 +10,25 @@ module Stevenson
       end
 
       def local_directory
-        @_local_directory ||= Dir.mktmpdir.tap do |dir|
-          # Clone the repo to a temporary directory for later use
-          ::Git.clone(template_url, dir).tap do |repo|
-            # Switch_branch if set
-            repo.checkout(options[:branch]) if options.has_key?(:branch)
+        @_local_directory ||= begin
+          @template_path ||= Dir.mktmpdir.tap do |dir|
+            # Clone the repo to a temporary directory for later use
+            ::Git.clone(template_url, dir).tap do |repo|
+              # Switch_branch if set
+              repo.checkout(options[:branch]) if options.has_key?(:branch)
+            end
           end
+
+          super
         end
       rescue ::Git::GitExecuteError
         # If the given repo URL is not valid, raise an exception
         raise InvalidTemplateException.new('Failed to clone the repository')
+      end
+
+      def close
+        FileUtils.rm_rf template_path if template_path
+        super
       end
     end
   end
